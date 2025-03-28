8db984774e88dabe2dc3bf8b7989996c7e812c83
kage1414 <kylejohnson92294@gmail.com>
2025-03-28 14:08:31 -0400

    make text fields white through theme

--- src/theme.ts
+++ src/theme.ts
@@ -6,6 +6,10 @@ const theme = createTheme({
     primary: { main: "#fba919" },
     secondary: { main: "#5c2d6b" },
   },
+  components: {
+    MuiTextField: { defaultProps: { style: { backgroundColor: "white" } } },
+    MuiSelect: { defaultProps: { style: { backgroundColor: "white" } } },
+  },
 });
 
 export default theme;
